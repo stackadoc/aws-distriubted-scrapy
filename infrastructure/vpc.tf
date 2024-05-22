3856f9f0519ce383f757a3caf253d86b0f4d7703
amin saffar <amin@stackadoc.com>
2024-05-22 10:06:40 +0200

    remove dns configurations

--- infrastructure/vpc.tf
+++ infrastructure/vpc.tf
@@ -7,8 +7,6 @@ locals {
 
 resource "aws_vpc" "main" {
   cidr_block           = "10.10.0.0/16"
-  enable_dns_hostnames = true
-  enable_dns_support   = true
   tags                 = { Name = "distributed-scraping-vpc" }
 }
 
